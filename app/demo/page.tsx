0d53c9c6958a51ed6ec43867e94c04e090cfbb2c
root <root@PAVILION.>
2025-04-17 19:27:54 +0000

    demo route added

---
+++ app/demo/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar"
+import Profile from "@/components/Profile"
+import Gadgets from "@/components/Gadgest"
+import Socials from "@/components/Socials"
+
+export default function GadgetsPage() {
+  return (
+    <main className="max-w-[800px] mx-auto pt-6 px-4 sm:px-6 lg:px-8">
+     <h1>Hello this is the demo route</h1>
+    </main>
+  )
+}
+
